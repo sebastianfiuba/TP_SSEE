8ba5e9814cfaa3c73aa56f7f156c8df69175319e
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-25 17:02:08 -0300

    Create realtime.h

---
+++ modules/realtime/realtime.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _REAL_TIME_H_
+#define _REAL_TIME_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+char* dateAndTimeRead();
+
+void dateAndTimeWrite( int year, int month, int day, 
+                       int hour, int minute, int second );
+
+//=====[#include guards - end]=================================================
+
+#endif // _REAL_TIME_H_
