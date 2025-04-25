735f251de355a601597150fec6646d2a9362e2bf
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-25 17:00:35 -0300

    Create locksystem.h

---
+++ modules/locksystem/locksystem.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _LOCK_SYSTEM_H_
+#define _LOCK_SYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+#define SYSTEM_TIME_INCREMENT_MS   10
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _LOCK_SYSTEM_H_
