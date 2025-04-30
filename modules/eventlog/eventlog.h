7ed20c5a1ca128b887826423c5d5d66fd49c768b
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 19:09:48 -0300

    Create eventlog.h

---
+++ modules/eventlog/eventlog.h
@@ -0,0 +1,15 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _EVENTLOG_H_
+#define _EVENTLOG_H_
+
+//=====[Declaration of public defines]=========================================
+
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _EVENTLOG_H_
