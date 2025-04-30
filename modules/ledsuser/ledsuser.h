2c00ae6da00d8ec8344dd32a3b057e7124803293
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 18:03:49 -0300

    Create ledsuser.h

---
+++ modules/ledsuser/ledsuser.h
@@ -0,0 +1,15 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _LEDSUSER_H_
+#define _LEDSUSER_H_
+
+//=====[Declaration of public defines]=========================================
+
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _LEDSUSER_H_
