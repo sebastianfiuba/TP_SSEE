4abe1dc9e5aaa8b26d5363b518c40b9bdbd69a5e
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 17:29:20 -0300

    Create buttonsuser.h

---
+++ modules/buttonsuser/buttonsuser.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _BUTTONSUSER_H_
+#define _BUTTONSUSER_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _BUTTONSUSER_H_
