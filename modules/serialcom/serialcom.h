3ae55151ef6b07fdb57605983a4a0c164596aff1
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 17:37:58 -0300

    Create serialcom.h

---
+++ modules/serialcom/serialcom.h
@@ -0,0 +1,15 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SERIALCOM_H_
+#define _SERIALCOM_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _SERIALCOM_H_
