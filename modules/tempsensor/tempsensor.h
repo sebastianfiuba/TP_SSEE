6b71d1fa2ef664491187d99efc37997bc75a53ce
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 17:38:41 -0300

    Create tempsensor.h

---
+++ modules/tempsensor/tempsensor.h
@@ -0,0 +1,16 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _TEMPSENSOR_H_
+#define _TEMPSENSOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _TEMPSENSOR_H_
