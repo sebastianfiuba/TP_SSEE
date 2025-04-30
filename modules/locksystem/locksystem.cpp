740b74313b26ffcb0844ad6346f7b2d33c967d52
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 17:36:16 -0300

    Update locksystem.cpp

--- modules/locksystem/locksystem.cpp
+++ modules/locksystem/locksystem.cpp
@@ -1,1 +1,32 @@
+//=====[Libraries]=============================================================
 
+#include "arm_book_lib.h"
+
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void smartHomeSystemInit(){
+  
+}
+
+void smartHomeSystemUpdate(){
+
+  delay(SYSTEM_TIME_INCREMENT_MS);
+}
+
+
+//=====[Implementations of private functions]==================================
