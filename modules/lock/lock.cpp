8ecbca0dd9f70949369b4fb55c1695b8eeb21d4d
sebastianfiuba <seandrada@fi.uba.ar>
2025-04-30 21:59:09 -0300

    Update lock.cpp

--- modules/lock/lock.cpp
+++ modules/lock/lock.cpp
@@ -1,1 +1,35 @@
+//=====[Libraries]=============================================================
 
+#include "arm_book_lib.h"
+#include "mbed.h"
+#include "eventlog.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void updateLock(log_t *locklog){
+
+  
+  bool statelock = checkLockConditions();
+
+  bool changeslock = checkChangesLockLog(locklog, stateLock);
+
+  if(changeslock){
+    changeLock();
+    updateLogLock(locklog, statelock);
+  }
+  return 
+}
