cebcc866202c84bf7452dda586dca9ac62c09148
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-02 19:02:01 -0300

    Update eventlog.cpp

--- modules/eventlog/eventlog.cpp
+++ modules/eventlog/eventlog.cpp
@@ -20,6 +20,129 @@
 
 //=====[Implementations of public functions]===================================
 
+void initLog(log_t *eventlog){
+  eventlog->but1 = false;
+  eventlog->but2 = false;
+  eventlog->led1 = !INIT_LOCK_VALUE;
+  eventlog->led2 = INIT_LOCK_VALUE;
+  eventlog->lock = INIT_LOCK_VALUE;
+  eventlog->changes = false;
+  eventlog->temp = 0;
+  eventlog->hum = 0;
+  eventlog->sens = 0;
+  
+  return;
+  
+}
+
+/*
+true if dif
+false if dif
+  */
+bool checkLogButtons(const log_t* firstcomp, const log_t* seccomp){
+
+  if(firstcomp->but1 != seccomp-> but1)
+    return true;
+  if(firstcomp->but2 != seccomp-> but2)
+    return true;
+  return false;
+}
+
+void makeLogButtons(log_t* logbuttons, const bool but1, const bool but2){
+
+  logbuttons->but1 = but1;
+  logbuttons->but2 = but2;
+  
+  return;
+}
+
+void updateLogButtons(log_t* logsource, log_t* logobj){
+
+  logobj->but1 = logsource->but1;
+  logobj->but2 = logsource->but2;
+
+  return;
+}
+
+/*
+true if dif
+false if dif
+*/
+
+bool checkChangesLockLog(const log_t* locklogcheck, const bool state){
+
+  if(locklogcheck->lock != state)
+    return true;
+  
+  return false;
+}
+
+
+void updateLogLock(log_t* locklogupd, const bool statelock){
+
+  locklogupd->lock = statelock;
+  
+  return;
+}
+
+
+void updateTempLog(log_t* sensorlog, int temp){
+
+  sensorlog->temp = temp;
+  
+  return;
+}
+
+void updateHumLog(log_t* sensorlog, int hum){
+
+  sensorlog->hum = hum;
+  
+  return;
+}
+bool getBut1Log(const log_t* elog){
+  
+  return elog->but1;
+}
+bool getBut2Log(const log_t* elog){
+  
+  return elog->but2;
+}
+
+bool getLed1Log(const log_t* elog){
+  
+  return elog->led1;
+}
+bool getLed2Log(const log_t* elog){
+  
+  return elog->led2;
+}
+int getSensLog(const log_t* elog){
+  
+  return elog->sens;
+}
+
+int getTempLog(const log_t* elog){
+  
+  return elog->temp;
+}
+
+int getHumLog(const log_t* elog){
+  
+  return elog->hum;
+}
+
+bool getLockLog(const log_t* elog){
+  
+  return elog->lock;
+}
+
+bool getChangesFlagLog(const log_t* elog){
+  
+  return elog->changes;
+}
+
+
+
 
 
 
