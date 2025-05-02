8493437aae34dcb8fc90b8a84103ade9f2b7c75b
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-01 21:02:34 -0300

    Update lock.cpp

--- modules/lock/lock.cpp
+++ modules/lock/lock.cpp
@@ -5,7 +5,8 @@
 #include "eventlog.h"
 
 //=====[Declaration of private defines]========================================
-
+#define LOW_LIMIT_TEMP 15
+#define HIGH_LIMIT_HUM 80
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
@@ -17,19 +18,38 @@
 //=====[Declaration and initialization of private global variables]============
 
 //=====[Declarations (prototypes) of private functions]========================
+bool checkLockConditions(log_t *lock){
 
+  if(getBut1(lock))
+    return false
+  if(getBut2(lock))
+    return true
+  float sens_aux = getSensLog(lock);
+  float temp_aux = getTempLog(lock);
+  if(LOW_LIMIT_TEMP >= temp_aux <= sens_aux)
+    return true
+  int hum = getHumLog(lock);
+  if(hum >= HIGH_LIMIT_HUM)
+    return true
+}
+
+void changeLock(bool state){
+
+  
+  return
+}
 //=====[Implementations of public functions]===================================
 
 void updateLock(log_t *locklog){
 
   
-  bool statelock = checkLockConditions();
+  bool statelock = checkLockConditions(locklog);
 
   bool changeslock = checkChangesLockLog(locklog, stateLock);
 
   if(changeslock){
-    changeLock();
-    updateLogLock(locklog, statelock);
+    changeLock(statelock);
+    updateLogLock(locklog, statelock); //changes true
   }
   return 
 }
