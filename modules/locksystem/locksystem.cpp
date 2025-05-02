774b385343747c5821392d1bea9ae4ce7ad8d94f
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-02 18:24:57 -0300

    Update locksystem.cpp

--- modules/locksystem/locksystem.cpp
+++ modules/locksystem/locksystem.cpp
@@ -2,15 +2,27 @@
 
 #include "arm_book_lib.h"
 
+#include "mbed.h"
+
+#include "eventlog.h"
+#include "buttonuse.h"
+#include "ledsuser.h"
+#include "lock.h"
+#include "potsens.h"
+#include "realtime.h"
+#include "serialcom.h"
+#include "tempsensor.h"
+#include "userinterface.h"
 
 //=====[Declaration of private defines]========================================
-
+#define DHTPIN D4
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
 
 //=====[Declaration of external public global variables]=======================
-
+log_t sys;
+dht11_t ths;
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
@@ -20,11 +32,20 @@
 //=====[Implementations of public functions]===================================
 
 void smartHomeSystemInit(){
-  
+
+  userInterfaceInit();
+  initLog(&sys);
+  initLock();
+  initSensor(&ths, DHTPIN);
+  pcSerialComInit();
 }
 
 void smartHomeSystemUpdate(){
 
+  userInterfaceUpdate(&sys);
+  updateSensor(&sys, &ths);
+  updateLock(&sys);
+  pcSerialComUpdate(&sys);
   delay(SYSTEM_TIME_INCREMENT_MS);
 }
 
