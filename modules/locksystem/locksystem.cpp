da577759beaa6c59a257c030625e3282c7f2cb4b
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-06 13:20:21 -0300

    Update locksystem.cpp

--- modules/locksystem/locksystem.cpp
+++ modules/locksystem/locksystem.cpp
@@ -4,16 +4,23 @@
 
 #include "mbed.h"
 
-#include "locksystem.h"
+
+#include "tempdht.h" 
+#include "tempsensor.h"
+
+
 #include "eventlog.h"
+
 #include "buttonsuser.h"
 #include "ledsuser.h"
 #include "lock.h"
 #include "potsens.h"
 #include "realtime.h"
 #include "serialcom.h"
-#include "tempsensor.h"
 #include "userinterface.h"
+
+#include "locksystem.h"
+
 
 //=====[Declaration of private defines]========================================
 #define DHTPIN D4
@@ -23,7 +30,7 @@
 
 //=====[Declaration of external public global variables]=======================
 log_t sys;
-dht11_t ths;
+//dht11_t ths;
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
@@ -37,17 +44,22 @@ void  locksysInit(){
   userInterfaceInit();
   initLog(&sys);
   initLock();
-  initSensor(&ths, DHTPIN);
+ // initSensor(&ths, DHTPIN);
   pcSerialComInit();
 }
 
 void locksysUpdate(){
 
   userInterfaceUpdate(&sys);
-  updateSensor(&sys, &ths);
+  //updateSensor(&sys, &ths);
+  
+  updateSensorDHT(&sys);
   updateLock(&sys);
+  userInterfaceUpdate(&sys);
   pcSerialComUpdate(&sys);
+  
   delay(SYSTEM_TIME_INCREMENT_MS);
+
 }
 
 
