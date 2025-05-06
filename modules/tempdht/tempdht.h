a486691281d5500fc21eabd844a42360a26f417c
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-06 13:29:03 -0300

    Create tempdht.h

---
+++ modules/tempdht/tempdht.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+#ifndef _TEMPSENSOR_H_
+#define _TEMPSENSOR_H_
+
+#include "eventlog.h"
+
+//=====[Declaration of public defines]=========================================
+
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void updateSensorDHT(log_t* sensorlog);
+
+//=====[#include guards - end]=================================================
+
+#endif // _TEMPSENSOR_H_
