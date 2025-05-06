ab96a6bdcb3ce3a1c99e3134677ca2290b13a13a
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-06 13:16:20 -0300

    Update buttonsuser.h

--- modules/buttonsuser/buttonsuser.h
+++ modules/buttonsuser/buttonsuser.h
@@ -4,14 +4,14 @@
 #define _BUTTONSUSER_H_
 
 //=====[Declaration of public defines]=========================================
-
+#include "eventlog.h"
 //=====[Declaration of public data types]======================================
 
 
 //=====[Declarations (prototypes) of public functions]=========================
 
 void initButtons();
-void updateButtons(log_t *events);
+void updateButtons(log_t* events);
 
 //=====[#include guards - end]=================================================
 
