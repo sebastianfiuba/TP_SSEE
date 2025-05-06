1d5c50e1e8cee25f01f13227f119c9672495e963
sebastianfiuba <seandrada@fi.uba.ar>
2025-05-06 13:18:33 -0300

    Update ledsuser.h

--- modules/ledsuser/ledsuser.h
+++ modules/ledsuser/ledsuser.h
@@ -4,12 +4,14 @@
 #define _LEDSUSER_H_
 
 //=====[Declaration of public defines]=========================================
-
+#include "eventlog.h"
 
 //=====[Declaration of public data types]======================================
 
 //=====[Declarations (prototypes) of public functions]=========================
 void updateUserleds(log_t *led);
+void initUserLeds();
+
 //=====[#include guards - end]=================================================
 
 #endif // _LEDSUSER_H_
